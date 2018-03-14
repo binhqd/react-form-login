a02af354166587dad661713c807ec1cad33c1197
Binh Quan <binhqd@gmail.com>
2018-03-14 16:46:05 +0700

    Chore: Add sample test script

---
+++ __tests__/LoginForm.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Enzyme, { mount, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { LoginForm } from '../build/bundle';
+
+
+Enzyme.configure({ adapter: new Adapter() });
+
+test('Welcome component should return correct output', () => {
+  const wrapper = mount(<LoginForm />);
+
+  expect(wrapper.html()).toEqual('<div>Welcome to React Component Boilerplate</div>');
+});
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 // this should be the entry point to your library
 module.exports = {
-  LoginForm: require('./LoginForm.jsx').default
+  LoginForm: require('./LoginForm').default
 };
+
+// import LoginForm from './LoginForm';
+//
+// export default LoginForm;
